65459f1bf317143a15b4741b7680400ba578cbd4
VarrinkalaPrasad <90542933+VarrinkalaPrasad@users.noreply.github.com>
2025-07-14 00:57:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('✅ Hello from Dockerized Node.js App!');
+  res.send('✅ Hello from Dockerized Node.js App! learning world');
 });
 
 app.listen(PORT, () => {
