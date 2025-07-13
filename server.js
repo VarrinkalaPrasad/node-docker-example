fc4738d41e960e73ed6077cb2a3e4af83f2f5f9f
VarrinkalaPrasad <90542933+VarrinkalaPrasad@users.noreply.github.com>
2025-07-14 00:26:00 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('✅ Hello from Dockerized Node.js App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
